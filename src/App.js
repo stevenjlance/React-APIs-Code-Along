50fa2586e5e2a2d2233ded780d23edb230e561c9
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2022-04-12 12:26:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ class App extends Component {
     super(props);
     this.state = {
       // Add data state here
+      data: []
     };
   }
 
   // Code your API request here
+  componentDidMount = () => {
+    fetch(
+      "https://data.cityofnewyork.us/resource/dpec-ucu7.json?$$app_token=2y6eroq5UDtsqbdRlo8SOA9rt&status=Incomplete"
+    );
+    // convert response to JSON
+    .then(response => response.json())
+    .then(data => {this.setState({data: data});
+    console.log(this.state.data)
+    })
+  
+  
+      
+  };
 
   render() {
     return (
       <div className="App">
         <h1>🚕 TAXI DRIVERS LICENSE STATUS 🚕</h1>
-        <section className="list">{/* PUT API RESPONSE DATA HERE */}</section>
+        <section className="list">{/* PUT API RESPONSE DATA HERE */}
+        {this.state.data.map(applicatio)=> (
+        <li>
+          application.app_no}
+          :
+          {application.other_requirments}</li>)}
+        </section>
       </div>
     );
   }
