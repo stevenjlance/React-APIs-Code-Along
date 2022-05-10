05ae199080c627a89a3979fd939540efb02ca59d
stevenjlance <44912347+stevenjlance@users.noreply.github.com>
2022-05-10 12:01:01 -0400

    Removed emojis

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>🚕 TAXI DRIVERS LICENSE STATUS 🚕</h1>
+        <h1>TAXI DRIVERS LICENSE STATUS </h1>
         <section className="list">{/* PUT API RESPONSE DATA HERE */}</section>
       </div>
     );
